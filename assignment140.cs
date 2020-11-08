e5de3d3400a891f5b7ef2328219f8c830bf669cd
Chad Okulovich <68598620+ELWChadrock@users.noreply.github.com>
2020-11-08 16:33:15 -0700

    Add files via upload

---
+++ assignment140.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace assignment140
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            string helpAnswer = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedbackAnswer = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hoursAnswer = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}
