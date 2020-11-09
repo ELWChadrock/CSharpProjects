9d8d8de5cbc176c4e84987f041d599158e138dc4
Chad Okulovich <68598620+ELWChadrock@users.noreply.github.com>
2020-11-08 19:44:27 -0700

    Add files via upload

---
+++ assignment158.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment158
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is a cool number?");
+            int answerWith = Convert.ToInt32(Console.ReadLine());
+            int product = answerWith * 50;
+            Console.WriteLine(product);
+            int total = answerWith + 25;
+            Console.WriteLine(total);
+            int quotient = (int)(answerWith / 12.5);
+            Console.WriteLine(quotient);
+            bool isOver = answerWith > 50;
+            Console.WriteLine(isOver);
+            int remainder = answerWith % 7;
+            Console.WriteLine(remainder);
+
+        }
+    }
+}
