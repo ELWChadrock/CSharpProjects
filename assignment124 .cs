4aecdcdcc266c0a61c23d60af35741244dd56de0
Chad Okulovich <68598620+ELWChadrock@users.noreply.github.com>
2020-11-14 20:03:43 -0700

    Add files via upload

--- assignment124 .cs
+++ assignment124 .cs
@@ -7,7 +7,7 @@ namespace myConsoleProject.cs
         static void Main()
         {
             Console.WriteLine("Welcome to Acme Accounting Systems");
-            Console.WriteLine("Remember, we're \" accounting \" on you!);
+            Console.WriteLine("Remember, we're\"accounting\"on you!");
             Console.Read();
         }
     }
